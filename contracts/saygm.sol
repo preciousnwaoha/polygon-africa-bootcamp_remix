08505ec85adcbe37488f0f827bb9142dd03cf152
Remix Workspace <>
2022-10-24 16:47:18 +0100

    "Implemented SayGM Contract"

--- contracts/saygm.sol
+++ contracts/saygm.sol
@@ -12,10 +12,39 @@ contract SayGM is Ownable {
         string answer;
     }
 
-    Answer[] public answers;
+    Answer[] answers;
 
     uint256 public tips;
 
-    
+    // emit an event whenever someone answers
+    event NewAnswer(address indexed from, string answer);
 
+    constructor() {}
+
+    function setPrompt(string memory newPrompt) public onlyOwner {
+        prompt = newPrompt;
+        delete answers;
+    }
+
+    function answer(string memory input) public {
+        // create answer struct and push into answer array
+        answers.push(Answer(msg.sender, input));
+        emit NewAnswer(msg.sender, input);
+    }
+
+    function getAnswers() public view returns (Answer[] memory) {
+        return answers;
+    }
+
+    function getPrompt() public view returns (string memory) {
+        return prompt;
+    }
+
+    function tip() public payable {
+        tips += msg.value;
+    }
+
+    function withdraw() public payable onlyOwner {
+        payable(msg.sender).transfer(address(this).balance); // security issues, so keep in mind
+    }
 }
